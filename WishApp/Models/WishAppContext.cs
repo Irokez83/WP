650f1975d61382afa295cc6679606fdf240acbb0
VJS <sigulda@hotmail.com>
2015-04-30 16:09:14 +0100

    version2

--- WishApp/Models/WishAppContext.cs
+++ WishApp/Models/WishAppContext.cs
@@ -8,14 +8,20 @@ namespace WishApp.Models
 {
     public class WishAppContext : DbContext
     {
-
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
         public WishAppContext() : base("name=WishAppContext")
         {
         }
 
-        public DbSet<User> Users { get; set; }
+        public System.Data.Entity.DbSet<WishApp.Models.User> Users { get; set; }
 
-        public DbSet<Preference> Preferences { get; set; }
-
+        public System.Data.Entity.DbSet<WishApp.Models.Preference> Preferences { get; set; }
+    
     }
 }
