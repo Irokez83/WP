bb2b54ad7866889c7c6958a7d362deae13f11a04
VJS <sigulda@hotmail.com>
2015-05-03 14:39:02 +0100

    webdesign

--- WishApp/App_Start/RouteConfig.cs
+++ WishApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WishApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Extra", action = "About", id = UrlParameter.Optional }
+                defaults: new { controller = "Extra", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
