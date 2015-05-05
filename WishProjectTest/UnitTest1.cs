09577f0d25c6e027106ee0699622cc6c99e0e477
niall300 <niall300@yahoo.co.uk>
2015-05-05 23:30:49 +0100

    code tidied, test project added

---
+++ WishProjectTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WishApp.Controllers;
+using WishApp.Models;
+using WishApp;
+
+
+namespace WishProjectTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class ProductControllerTest
+        {
+            [TestMethod]
+            public void TestDetailsView()
+            {
+                //create a controller instance
+                var controller = new UsersController();
+                //call the index method
+                var result = controller.Index();
+                //check does it return the index view
+                //not working assertion needs correcting
+                Assert.AreEqual("Index", result);
+
+            }
+        }
+    }
+}
