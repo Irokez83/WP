e6186965e31aa4b915bdc9d4c3693da11e722001
VJS <sigulda@hotmail.com>
2015-05-05 23:35:19 +0100

    UT

---
+++ WishProjectTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WishApp.Controllers;
+
+namespace WishProjectTest
+{
+    [TestClass]
+public class UnitTest1
+{
+
+    [TestMethod]            
+public void Withdraw_AmountMoreThanBalance_Throws()
+{
+    //string[] str = { "value1", "value2"};
+    // arrange
+    //UsersAPIController ctl = new UsersAPIController();
+    // act
+    //string[] actual = ct1.get();
+    // assert is handled by the ExpectedException
+    //Assert.AreEqual(str, actual);
+}
+}
+}
+
